82565ebc86dd89e934f9a7fe574c006261af2c4c
Alex Berezovskiy <alex.b.2658@gmail.com>
2018-07-13 17:25:49 +0300

    Add a type annotation to each val in SwaggerRoute

--- src/main/scala/org/encryfoundation/explorer/http/api/routes/SwaggerRoute.scala
+++ src/main/scala/org/encryfoundation/explorer/http/api/routes/SwaggerRoute.scala
@@ -4,18 +4,19 @@ import akka.actor.ActorRefFactory
 import akka.http.scaladsl.server.Route
 import com.github.swagger.akka.SwaggerHttpService
 import com.github.swagger.akka.model.Info
-import io.swagger.models.auth.BasicAuthDefinition
+import io.swagger.models.ExternalDocs
+import io.swagger.models.auth.{BasicAuthDefinition, SecuritySchemeDefinition}
 import org.encryfoundation.explorer.db.models._
 import org.encryfoundation.explorer.settings.RESTApiSettings
 
 case class SwaggerRoute(settings: RESTApiSettings)(implicit val context: ActorRefFactory) extends SwaggerHttpService with ApiRoute {
 
-  override val apiClasses = Set(classOf[HistoryRoute], classOf[TransactionsApiRoute])
-  override val host = "localhost:9052"
-  override val info = Info(version = "0.1" )
-  override val externalDocs = None //Some(new ExternalDocs("Core Docs", "http://acme.com/docs"))
-  override val securitySchemeDefinitions = Map("basicAuth" -> new BasicAuthDefinition())
-  override val unwantedDefinitions = Seq("Function1", "Function1RequestContextFutureRouteResult")
+  override val apiClasses: Set[Class[_]] = Set(classOf[HistoryRoute], classOf[TransactionsApiRoute])
+  override val host: String = "localhost:9052"
+  override val info: Info = Info(version = "0.1" )
+  override val externalDocs: Option[ExternalDocs] = None
+  override val securitySchemeDefinitions: Map[String, SecuritySchemeDefinition] = Map("basicAuth" -> new BasicAuthDefinition())
+  override val unwantedDefinitions: Seq[String] = Seq("Function1", "Function1RequestContextFutureRouteResult")
 
   def route: Route = withCors(super.routes)
 
