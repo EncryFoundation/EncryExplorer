6aaf09332e6bdcce1b1d3c3a0f8498a7b8586115
timofey <gusev.t.s1@gmail.com>
2018-07-18 17:37:24 +0300

    first push

--- app/LoggingSystem/LoggingAction.scala
+++ app/LoggingSystem/LoggingAction.scala
@@ -6,7 +6,7 @@ import play.api.mvc.{ActionBuilderImpl, BodyParsers, Request, Result}
 import scala.concurrent.{ExecutionContext, Future}
 
 class LoggingAction @Inject()(parser: BodyParsers.Default)(implicit ec: ExecutionContext) extends ActionBuilderImpl(parser) {
-  override def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]): Future[Result] = {
+  override def invokeBlock[A](request: Request[A], block: Request[A] => Future[Result]): Future[Result] = {
     Logger.info(s"Method = ${request.method},  " +
                 s"URI = ${request.uri},  " +
                 s"remote-address = ${request.remoteAddress},  " +
---
+++ app/controllers/HistoryController.scala
@@ -0,0 +1,15 @@
+package controllers
+
+import LoggingSystem.LoggingAction
+import javax.inject.{Inject, Singleton}
+import play.api.mvc._
+import play.mvc.Action
+
+@Singleton
+class HistoryController @Inject() (loggingAction: LoggingAction, cc: ControllerComponents)
+  extends AbstractController(cc) {
+
+  def getHeaderR: Action[AnyContent] = loggingAction { implicit request: Request[AnyContent] =>
+
+  }
+}
