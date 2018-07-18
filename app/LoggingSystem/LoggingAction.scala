b14433de46bc23fca03d6e48c6e70008b3b0b66e
timofey <gusev.t.s1@gmail.com>
2018-07-18 15:23:44 +0300

    code cleanup

---
+++ app/LoggingSystem/LoggingAction.scala
@@ -0,0 +1,16 @@
+package LoggingSystem
+
+import javax.inject.Inject
+import play.api.Logger
+import play.api.mvc.{ActionBuilderImpl, BodyParsers, Request, Result}
+import scala.concurrent.{ExecutionContext, Future}
+
+class LoggingAction @Inject()(parser: BodyParsers.Default)(implicit ec: ExecutionContext) extends ActionBuilderImpl(parser) {
+  override def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]): Future[Result] = {
+    Logger.info(s"Method = ${request.method},  " +
+                s"URI = ${request.uri},  " +
+                s"remote-address = ${request.remoteAddress},  " +
+                s"domain = ${request.domain}")
+    block(request)
+  }
+}
--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,9 +1,8 @@
 package controllers
 
+import LoggingSystem.LoggingAction
 import javax.inject.{Inject, _}
-import play.api.Logger
 import play.api.mvc._
-import scala.concurrent.{ExecutionContext, Future}
 
 /**
   * This controller creates an `Action` to handle HTTP requests to the
@@ -22,16 +21,5 @@ class HomeController @Inject() (loggingAction: LoggingAction, cc: ControllerComp
 
   def index() = loggingAction { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
-  }
-}
-
-class LoggingAction @Inject() (parser: BodyParsers.Default) (implicit ec: ExecutionContext) extends ActionBuilderImpl(parser) {
-  override def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]) = {
-    Logger.info(s"Method = ${request.method},  " +
-      s"URI = ${request.uri},  " +
-      s"Remote-address = ${request.remoteAddress},  " +
-      s"Domain = ${request.domain},  " +
-      s"User-agent = [${request.headers.get("user-agent").getOrElse("N/A")}]")
-    block(request)
   }
 }
