8fa27ff6094a6ce605b9180b588badad85be96a8
Stanislav Kapinus <kapinuss@rambler.ru>
2018-07-18 11:03:11 +0300

    -comments

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -5,28 +5,17 @@ import play.api.Logger
 import play.api.mvc._
 import scala.concurrent.{ExecutionContext, Future}
 
-/**
-  * This controller creates an `Action` to handle HTTP requests to the
-  * application's home page.
-  */
 @Singleton
-class HomeController @Inject() (loggingAction: LoggingAction, cc: ControllerComponents) extends AbstractController(cc) with ControllerHelpers {
+class HomeController @Inject()(loggingAction: LoggingAction, cc: ControllerComponents)
+  extends AbstractController(cc) with ControllerHelpers {
 
-  /**
-    * Create an Action to render an HTML page.
-    *
-    * The configuration in the `routes` file means that this method
-    * will be called when the application receives a `GET` request with
-    * a path of `/`.
-    */
-
-  def index() = loggingAction { implicit request: Request[AnyContent] =>
+  def index(): Action[AnyContent] = loggingAction { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
 }
 
-class LoggingAction @Inject() (parser: BodyParsers.Default) (implicit ec: ExecutionContext) extends ActionBuilderImpl(parser) {
-  override def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]) = {
+class LoggingAction @Inject()(parser: BodyParsers.Default)(implicit ec: ExecutionContext) extends ActionBuilderImpl(parser) {
+  override def invokeBlock[A](request: Request[A], block: Request[A] => Future[Result]): Future[Result] = {
     Logger.info(s"Method = ${request.method},  " +
       s"URI = ${request.uri},  " +
       s"Remote-address = ${request.remoteAddress},  " +
