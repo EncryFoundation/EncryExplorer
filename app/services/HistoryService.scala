3194461379e8fbe847f1705609da846329011207
Stanislav Kapinus <kapinuss@rambler.ru>
2018-07-25 15:02:56 +0300

    formatting

--- app/services/HistoryService.scala
+++ app/services/HistoryService.scala
@@ -7,22 +7,24 @@ import scala.concurrent.{ExecutionContext, Future}
 
 class HistoryService @Inject()(historyDao: HistoryDao)(implicit ec: ExecutionContext) {
 
-  def getHeader(id: String): Future[Option[Header]] = {
-    Future
-      .fromTry(Base16.decode(id))
+  def getHeader(id: String): Future[Option[Header]] =
+    Future.fromTry(Base16.decode(id))
       .flatMap(_ => historyDao.findHeader(id))
-  }
 
   def getHeadersAtHeight(height: Int): Future[List[Header]] =
-    if (height >= 0) historyDao.findHeadersAtHeight(height) else Future.failed(new IllegalArgumentException)
+    if (height >= 0) historyDao.findHeadersAtHeight(height)
+    else Future.failed(new IllegalArgumentException)
 
   def getBestHeaderAtHeight(height: Int): Future[Option[Header]] =
-    if (height >= 0) historyDao.findBestHeadersAtHeight(height) else Future.failed(new IllegalArgumentException)
+    if (height >= 0) historyDao.findBestHeadersAtHeight(height)
+    else Future.failed(new IllegalArgumentException)
 
   def getLastHeaders(qty: Int): Future[List[Header]] =
-    if (qty >= 0) historyDao.findLastHeaders(qty) else Future.failed(new IllegalArgumentException)
+    if (qty >= 0) historyDao.findLastHeaders(qty)
+    else Future.failed(new IllegalArgumentException)
 
   def getHeadersByHeightRange(from: Int, to: Int): Future[List[Header]] =
-    if (from >= 0 && to >= from) historyDao.findByRange(from, to) else Future.failed(new IllegalArgumentException)
+    if (from >= 0 && to >= from) historyDao.findByRange(from, to)
+    else Future.failed(new IllegalArgumentException)
 
 }
