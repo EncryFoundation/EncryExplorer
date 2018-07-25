b095aa91684f42fe0ef5b6a214f413eb1793e775
Stanislav Kapinus <kapinuss@rambler.ru>
2018-07-25 15:15:33 +0300

    formatting

--- app/models/database/HeadersQueryRepository.scala
+++ app/models/database/HeadersQueryRepository.scala
@@ -6,14 +6,19 @@ import models.Header
 
 object HeadersQueryRepository {
 
-  def findByIdQuery(id: String): ConnectionIO[Option[Header]] = sql"SELECT * FROM headers WHERE id = $id".query[Header].to[List].map(_.headOption)
+  def findByIdQuery(id: String): ConnectionIO[Option[Header]] =
+    sql"SELECT * FROM headers WHERE id = $id".query[Header].to[List].map(_.headOption)
 
-  def findByHeightQuery(height: Int): ConnectionIO[List[Header]] = sql"SELECT * FROM headers WHERE height = $height".query[Header].to[List]
+  def findByHeightQuery(height: Int): ConnectionIO[List[Header]] =
+    sql"SELECT * FROM headers WHERE height = $height".query[Header].to[List]
 
-  def findBestByHeightQuery(height: Int): ConnectionIO[Option[Header]] = sql"SELECT * FROM headers WHERE height = $height AND best_chain = TRUE".query[Header].to[List].map(_.headOption)
+  def findBestByHeightQuery(height: Int): ConnectionIO[Option[Header]] =
+    sql"SELECT * FROM headers WHERE height = $height AND best_chain = TRUE".query[Header].to[List].map(_.headOption)
 
-  def findLastByHeightQuery(height: Int): ConnectionIO[List[Header]] = sql"SELECT * FROM headers WHERE best_chain = TRUE ORDER BY height DESC LIMIT $height".query[Header].to[List]
+  def findLastByHeightQuery(height: Int): ConnectionIO[List[Header]] =
+    sql"SELECT * FROM headers WHERE best_chain = TRUE ORDER BY height DESC LIMIT $height".query[Header].to[List]
 
-  def findByHeightRangeQuery(from:Int, to: Int): ConnectionIO[List[Header]] = sql"SELECT * FROM headers WHERE height BETWEEN $from AND $to".query[Header].to[List]
+  def findByHeightRangeQuery(from:Int, to: Int): ConnectionIO[List[Header]] =
+    sql"SELECT * FROM headers WHERE height BETWEEN $from AND $to".query[Header].to[List]
 }
 
