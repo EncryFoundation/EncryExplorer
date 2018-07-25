0452b32cf6e425bdb68bc3a0e8efe2723567482d
YERMLV <germolaev@innodata.ru>
2018-07-25 17:16:12 +0300

    Added block controller

---
+++ app/controllers/BlockController.scala
@@ -0,0 +1,28 @@
+package controllers
+
+import io.circe.syntax._
+import javax.inject.Inject
+import models.Block
+import play.api.libs.circe.Circe
+import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
+import services.{HistoryService, TransactionsService}
+
+import scala.concurrent.ExecutionContext
+
+class BlockController @Inject()
+(cc: ControllerComponents, historyService: HistoryService, transactionsService: TransactionsService)(implicit ex: ExecutionContext)
+  extends AbstractController(cc) with Circe {
+
+  def findBlock(id: String): Action[AnyContent] = Action.async {
+    val headerFuture = historyService.getHeader(id)
+    val payloadFuture = transactionsService.listTransactionsByBlockId(id)
+    for {
+      headerOpt <- headerFuture
+      payload <- payloadFuture
+    } yield headerOpt match {
+      case Some(header) => Ok(Block(header, payload).asJson)
+      case None => NotFound
+    }
+  }
+
+}
