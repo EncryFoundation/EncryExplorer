594fd26ea4ddf83342b90498ce4a1fc651d957a2
lior <lior.liviev@mail.ru>
2018-10-04 10:16:58 +0300

    added new fields to db

--- app/models/Transaction.scala
+++ app/models/Transaction.scala
@@ -1,6 +1,7 @@
 package models
 
 case class Transaction(id: String,
+                       number_in_block: Int,
                        fee: Long,
                        blockId: String,
                        isCoinbase: Boolean,
